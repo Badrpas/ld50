bc888582d12800a9c8c8a09d413e2709f18171cc
Vadim Samarin <dezivert@gmail.com>
2022-04-02 18:59:06 +0300

    Camera mouse move

--- pkg/game/camera.go
+++ pkg/game/camera.go
@@ -3,6 +3,7 @@ package game
 import (
 	"github.com/badrpas/ld50/pkg/entity"
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
 	camera "github.com/melonfunction/ebiten-camera"
 )
 
@@ -10,6 +11,9 @@ func init_camera(g *Game) {
 	width, height := ebiten.WindowSize()
 	g.Camera = camera.NewCamera(width, height, 0, 0, 0, 1)
 
+	const MouseMoveBtn = ebiten.MouseButtonRight
+
+	sx, sy := 0, 0
 	handler := &entity.Entity{
 		Update: func(self *entity.Entity, dt float64) {
 			cam_delta := dt * 400
@@ -26,7 +30,19 @@ func init_camera(g *Game) {
 			if ebiten.IsKeyPressed(ebiten.KeyS) {
 				g.Camera.MovePosition(0, cam_delta)
 			}
+
+			if inpututil.IsMouseButtonJustPressed(MouseMoveBtn) {
+				sx, sy = ebiten.CursorPosition()
+			}
+
+			if ebiten.IsMouseButtonPressed(MouseMoveBtn) {
+				x, y := ebiten.CursorPosition()
+				dx, dy := -x+sx, -y+sy
+				sx, sy = x, y
+				g.Camera.MovePosition(float64(dx), float64(dy))
+			}
 		},
 	}
+
 	g.AddEntity(handler)
 }
